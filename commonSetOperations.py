3ebaa6710f2c167ccc2291edd75e6b60818fe0dd
Mark Peters <mapeters101@earthlink.net>
2024-10-16 00:14:05 -0400

    Lesson090: work with common set operations up to 11:00.

--- commonSetOperations.py
+++ commonSetOperations.py
@@ -42,5 +42,38 @@ print(s1 | s2)
 print(s1 & s2)
 print(s1 - s2)
 print(s2 - s1)
+print('+'*20)
+
+str_1 = 'python is an awesome language!'
+str_2 = 'a python is also a snake.'
+
+set_1 = set(str_1)
+set_2 = set(str_2)
+
+print(set_1)
+print(set_2)
+print(set_1 & set_2)
+print('+'*20)
+
+s1 = {'FB', 'AMZN', 'AAPL', 'NFLX', 'GOOG', 'MSFT'}
+s2 = {'BABA', 'WMT', 'COST'}
+s3 = {'TSLA', 'F', 'GM'}
+
+consolidated = s1 | s2 | s3
+print(consolidated)
+
+consolidated = list(s1 | s2 | s3)
+print(consolidated)
+
+sold = {'w1', 'w2', 'w3', 'w4'}
+returned = {'w1'}
+non_returned = sold - returned
+print(non_returned)
+print('+'*20)
+
+alphabet = set('abcdefghijklmnopqrstuvwxyz')
+import string
+
+
 
 
